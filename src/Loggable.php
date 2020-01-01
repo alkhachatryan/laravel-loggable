363deae84a89cf92bd8ec7dab5ebe148374164a1
alexey <alexey.khachatryan@gmail.com>
2020-01-02 02:30:04 +0400

    Created trait with eloquent observer events and logger class calling

---
+++ src/Loggable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Alkhachatryan\LaravelLoggable;
+
+use App\Exceptions\LoggableFieldsNotSetException;
+
+trait Loggable
+{
+    /**
+     * Register eloquent events of model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        self::created(function($model)
+        {
+            self::log($model, 'create');
+        });
+
+        self::updated(function($model)
+        {
+            self::log($model, 'edit');
+        });
+
+        self::deleted(function($model)
+        {
+            self::log($model, 'delete');
+        });
+    }
+
+    /**
+     * Create and call the logger base class with collected data.
+     *
+     * @throws LoggableFieldsNotSetException
+     * @param mixed $model
+     * @param string $action
+     * @return void
+    */
+    private static function log($model, $action){
+        $logger = new Logger($model, $action);
+        $logger->record();
+    }
+}
